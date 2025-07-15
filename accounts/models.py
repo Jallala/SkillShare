fe36e8a0cffb33241699dbc70a97b96a73aee24a
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-07-15 09:54:00 +0200

    remove unnecessary code from models

--- accounts/models.py
+++ accounts/models.py
@@ -5,8 +5,6 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
-    skills_offered = models.CharField(max_length=255, blank=True)
-    skills_needed = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
