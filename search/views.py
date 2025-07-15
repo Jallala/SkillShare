5e81335a1c370c5022be25be58483c6893a871e6
sputgarin <89846079+sputgarin@users.noreply.github.com>
2025-07-15 13:11:34 +0200

    was working on a more sophisticated search with user:username but had to discard it. back to square 1

--- search/views.py
+++ search/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from skillswap_common.models import Skill
 
 def skill_list(request):
-    skills = Skill.objects.all()
-    return render(request, 'search/skill_list.html', {'skills': skills})
+    query = request.GET.get('q', '').strip() 
+    users = []
+    skills = []
 
+    if query:  
+        users = User.objects.filter(username__icontains=query)
+        skills = Skill.objects.filter(title__icontains=query)
+
+
+    return render(request, 'search/skill_list.html', {
+        'users': users,
+        'skills': skills,
+        'query': query
+    })
