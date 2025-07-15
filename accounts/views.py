60c3d5067ad32534470c84fd3a784108792e2db4
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-07-15 09:55:59 +0200

    add skills model from skillswap_app

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,7 @@ from .models import Profile
 from .forms import ProfileForm, UserForm
 from django.contrib import messages
 from .forms import CustomUserCreationForm
+from skillswap_app.models import Skill
 
 
 from django.conf import settings
@@ -31,9 +32,14 @@ def signup_view(request):
 @login_required
 def profile_view(request):
     profile = request.user.profile
+    skills_offered = Skill.objects.filter(user=request.user, type='offer')
+    skills_needed = Skill.objects.filter(user=request.user, type='request')
+
     return render(request, 'accounts/profile.html', {
         'profile': profile,
-        'user_is_owner': request.user == profile.user
+        'user_is_owner': request.user == profile.user,
+        'skills_offered': skills_offered,
+        'skills_needed': skills_needed,
     })
 
 
