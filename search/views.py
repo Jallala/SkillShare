9e79e3e0be4554ac2058219c83cecff98ea57791
sputgarin <89846079+sputgarin@users.noreply.github.com>
2025-07-16 13:40:44 +0200

    Added my code back that was removed

--- search/views.py
+++ search/views.py
@@ -3,14 +3,13 @@ from django.contrib.auth.models import User
 from skillswap_common.models import Skill
 
 def skill_list(request):
-    query = request.GET.get('q', '').strip() 
+    query = request.GET.get('q', '').strip()
     users = []
     skills = []
 
-    if query:  
+    if query:
         users = User.objects.filter(username__icontains=query)
         skills = Skill.objects.filter(title__icontains=query)
-
 
     return render(request, 'search/skill_list.html', {
         'users': users,
