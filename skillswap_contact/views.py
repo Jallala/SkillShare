106d0a5041c5dc73616b6777a59c8cd397d8862d
Jonatan Ojala <jonatan.ojala@gmail.com>
2025-07-17 11:27:41 +0200

    use helper instad

--- skillswap_contact/views.py
+++ skillswap_contact/views.py
@@ -64,8 +64,7 @@ def messages(request: 'HttpRequest') -> HttpResponse:
 @login_required
 def chat_with_user(request: 'HttpRequest', uid: int) -> HttpResponse:
     all_messages = Messages.get_messages_for(request.user)
-    user = User.objects.get(pk=uid)
-    other_user = UserProfile.objects.get(user=user)
+    other_user = UserProfile.get_user_profile_from(uid)
     messages_with_user = all_messages.get_chat_log_with(uid)
     if request.method == 'POST':
         try:
